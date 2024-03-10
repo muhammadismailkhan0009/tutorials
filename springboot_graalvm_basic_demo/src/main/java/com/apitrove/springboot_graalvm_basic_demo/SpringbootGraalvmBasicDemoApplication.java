0150d4b799c568de08add53d325832f62364fd80
muhammadismailkhan0009 <muhammadismailkhan0009@gmail.com>
2024-03-10 18:54:59 +0500

    - initial command

---
+++ springboot_graalvm_basic_demo/src/main/java/com/apitrove/springboot_graalvm_basic_demo/SpringbootGraalvmBasicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.apitrove.springboot_graalvm_basic_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraalvmBasicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraalvmBasicDemoApplication.class, args);
+	}
+
+}
